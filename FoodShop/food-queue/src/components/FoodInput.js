60bcbab18e90ebfee86c440ad6443df8dd9eed0a
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-03-28 23:50:12 +0700

    [Add]Components
    -FoodInput
    -FoodItem
    -FoodList

---
+++ FoodShop/food-queue/src/components/FoodInput.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react';
+
+
+ class FoodInput extends Component{
+  render(){
+    return(
+        <div>FoodInput</div>
+      )
+     }
+
+  }
+  
+export default FoodInput;
---
+++ FoodShop/food-queue/src/components/FoodItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FoodItem extends Component {
+  render() {
+    return (
+      <div>FoodItem</div>
+    )
+
+  }
+}
+
+export default FoodList;
