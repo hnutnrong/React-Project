c66703a51c294f25e80c27135fbeb0ac2464cf42
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-03-29 14:36:39 +0700

    [Add]basic state

--- React&Redux/basicreact-redux/src/index.js
+++ React&Redux/basicreact-redux/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// serviceWorker.unregister();
+const reducer = (state, action) => {
+  switch (action.type) {  //action ใช้ในการเปลี่ยนแปลงค่า
+    case "ADD":  //ถ้าเป็น ADD ให้บวก
+        state = state + action.payload;
+      break;
+    case "SUBTRACT":  //ถ้าเป็น subtract ให้ -
+        state = state - action.payload;
+      break;
+    default:
+  }
+  return state;
+}
+
+const store = createStore(reducer, 15000); ///15000คือค่า State เอามาเก็ฐไว้ใน Store
+
+store.subscribe(()=>{  ///ใช่อัพเดทค่า State แสดงค่า State
+    console.log("Update Store:",store.getState());
+})
+store.dispatch({   ///.dispatch ใช้ในการเปลี่ยนแปลงค่าใน State
+      type:"ADD",   /// ดู action.type ว่าเป็นอะไรถ้าเป็น "ADD" มันก็จะไปตาม Case  acion.payload คือค่าที่เราส่งไปเผื่อเปลี่ยนแปลงค่า State
+      payload:500  ///ค่าที่เราส่งไปเพื่อทำการเปลี่ยนแปลงค่า State
+  
+});
+
+store.dispatch({
+      type:"SUBTRACT",
+      payload:1000
+});
