fb06f939c738b02b7aa465dc84464af51e89cf7e
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-03-30 00:42:17 +0700

    Revert "[Add]Redux-React(MapstatetoProps)"
    This reverts commit 4370c0f8abd9a9734bd42c41ee7296ab2ae722ee.

--- React&Redux/basicreact-redux/src/App.js
+++ React&Redux/basicreact-redux/src/App.js
@@ -1,39 +1,13 @@
 import React, { Component } from 'react';
-import User from './User.js';
-import { connect } from 'react-redux';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div>
-        <User
-          username={this.props.user.name}>
-        </User>
-        <button onClick={() => this.props.setName("NutChangeName")}>ChangeName</button> 
-      </div>
+    <h1>Nut</h1>
     );
+   }
   }
-}
-//สร้างComponent ส่งค่า properties ที่มีชื่อว่า username 
-//{this.props.user.name}มาจาก mapStatetoProps 
-//  onClick เป็นAction ให้ทำเวลากดปุ่ม
 
-const mapStatetoProps = (state) => {    //เปลี่ยน Stateใน index.js มาเป็น Properties
-  return {
-    user: state.user,   //user มาจาก State user  ที่อยู่ใน userReducer ใน index.js
-    emp: state.emp      //user มาจาก State user  ที่อยู่ใน employeeReducer ใน index.js
-  };
-}
-
-const mapDispatchtoProps = (dispatch) => {  //เปลี่ยนแปลงค่าใน State
-  return {
-    setName: (name) => {
-      dispatch({
-        type: "setName",
-        payload: name
-      });
-    }
-  };
-}
-
-export default connect(mapStatetoProps, mapDispatchtoProps)(App); 
+export default App;
