4370c0f8abd9a9734bd42c41ee7296ab2ae722ee
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-03-29 17:47:57 +0700

    [Add]Redux-React(MapstatetoProps)

--- React&Redux/basicreact-redux/src/App.js
+++ React&Redux/basicreact-redux/src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import User from './User.js';
+import { connect } from 'react-redux';
 
 class App extends Component {
-  render(){
+  render() {
     return (
-    <h1>Nut</h1>
+      <div>
+        <User
+          username={this.props.user.name}>
+        </User>
+        <button onClick={() => this.props.setName("NutChangeName")}>ChangeName</button> 
+      </div>
     );
-   }
   }
+}
+//สร้างComponent ส่งค่า properties ที่มีชื่อว่า username 
+//{this.props.user.name}มาจาก mapStatetoProps 
+//  onClick เป็นAction ให้ทำเวลากดปุ่ม
 
-export default App;
+const mapStatetoProps = (state) => {    //เปลี่ยน Stateใน index.js มาเป็น Properties
+  return {
+    user: state.user,   //user มาจาก State user  ที่อยู่ใน userReducer ใน index.js
+    emp: state.emp      //user มาจาก State user  ที่อยู่ใน employeeReducer ใน index.js
+  };
+}
+
+const mapDispatchtoProps = (dispatch) => {  //เปลี่ยนแปลงค่าใน State
+  return {
+    setName: (name) => {
+      dispatch({
+        type: "setName",
+        payload: name
+      });
+    }
+  };
+}
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(App); 
--- React&Redux/basicreact-redux/src/index.js
+++ React&Redux/basicreact-redux/src/index.js
@@ -1,19 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-import { createStore,combineReducers } from 'redux';  /// combineReducers ในกรณีมี Ruducer หลายตัว
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore, combineReducers } from 'redux';  /// combineReducers ในกรณีมี Ruducer หลายตัว
+import { Provider } from 'react-redux';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-
-// serviceWorker.unregister();
 
 const initialState = {
   result: 15000,
@@ -24,15 +15,15 @@ const userReducer = (state = { name: "Nutnarong", age: 21 }, action) => {
   switch (action.type) {
     case "setName":
       state = {
-          ...state,
-          name:action.payload
+        ...state,
+        name: action.payload
       }
       break;
 
     case "setAge":
       state = {
-          ...state,
-          age:action.payload
+        ...state,
+        age: action.payload
       }
       break;
     default:
@@ -47,7 +38,6 @@ const employeeReducer = (state = initialState, action) => {
       state = {
         // result:state.result,  
         // value:state.value
-
         ...state,
         //หรือจะเขียนแบบนี้ก็ได้
         result: state.result += action.payload,
@@ -61,7 +51,6 @@ const employeeReducer = (state = initialState, action) => {
       state = {
         // result:state.result,  
         // value:state.value
-
         ...state,
         //หรือจะเขียนแบบนี้ก็ได้
         result: state.result -= action.payload,
@@ -74,24 +63,34 @@ const employeeReducer = (state = initialState, action) => {
   return state;
 }
 
-const store = createStore(combineReducers({employeeReducer,userReducer})); //มี Reducer หลายตัว ใช้ combineReducer อย่าลืม import ด้วย
+const store = createStore(combineReducers({emp:employeeReducer,user:userReducer })); 
+//มี Reducer หลายตัว ใช้ combineReducer อย่าลืม import ด้วย
+//emp,user กำหนดตัวแปรไปใช้ mapStatetoProps ใน App.js
 
 store.subscribe(() => {  ///ใช่อัพเดทค่า State แสดงค่า State
   console.log("Update Store:", store.getState());
 })
-store.dispatch({   ///.dispatch ใช้ในการเปลี่ยนแปลงค่าใน State
-  type: "ADD",   /// ดู action.type ว่าเป็นอะไรถ้าเป็น "ADD" มันก็จะไปตาม Case  acion.payload คือค่าที่เราส่งไปเผื่อเปลี่ยนแปลงค่า State
-  payload: 15000  ///ค่าที่เราส่งไปเพื่อทำการเปลี่ยนแปลงค่า State
+// store.dispatch({   ///.dispatch ใช้ในการเปลี่ยนแปลงค่าใน State
+//   type: "ADD",     /// ดู action.type ว่าเป็นอะไรถ้าเป็น "ADD" มันก็จะไปตาม Case  acion.payload คือค่าที่เราส่งไปเผื่อเปลี่ยนแปลงค่า State
+//   payload: 15000   ///ค่าที่เราส่งไปเพื่อทำการเปลี่ยนแปลงค่า State
 
-});
+// });
 
-store.dispatch({ ///.dispatch ใช้ในการเปลี่ยนแปลงค่าใน State
-  type:"setName",
-  payload:"Redux"
-})
+// store.dispatch({    ///.dispatch ใช้ในการเปลี่ยนแปลงค่าใน State
+//   type:"setName",
+//   payload:"Redux"
+// })
 
-store.dispatch({ ///.dispatch ใช้ในการเปลี่ยนแปลงค่าใน State
-  type:"setAge",
-  payload:"22"
-})
+// store.dispatch({    ///.dispatch ใช้ในการเปลี่ยนแปลงค่าใน State
+//   type:"setAge",
+//   payload:"22"
+// })
+
+ReactDOM.render(                  //Provider เราจะนำ state ใน store มาทำงานร่วมกับ component
+  <Provider store={store}>  
+    <App />
+  </Provider>
+
+  ,document.getElementById('root')
+);
 
